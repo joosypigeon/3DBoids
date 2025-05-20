0aabc178c63e75a9dcc1c239df5fdc47f4a02be9
joosypigeon <joosypigeon@googlemail.com>
2025-05-20 15:56:50 +0100

    update GUI

--- src/main.c
+++ src/main.c
@@ -229,26 +229,17 @@ int main(void)
             DrawText(TextFormat("Frame Time: %0.2f ms", GetFrameTime() * 1000), 20, 110, 30, BLUE);
             DrawText(TextFormat("OpenMP threads: %d", omp_get_max_threads()), 20, 140, 30, BLUE);
 
-            int oldTextSize = GuiGetStyle(DEFAULT, TEXT_SIZE);
-            GuiSetStyle(DEFAULT, TEXT_SIZE, 24);
-            GuiCheckBox((Rectangle){ 500, 10, 28, 28 }, "Draw Full Boid Glyph", &drawFullGlyph);
-            GuiCheckBox((Rectangle){ 500, 40, 28, 28 }, "Show density", &drawDensity);
-            GuiCheckBox((Rectangle){ 500, 70, 28, 28 }, "Show nearest neighbours", &nearestNeighboursNetwork);
 
-            GuiSetStyle(DEFAULT, TEXT_SIZE, oldTextSize);  // Restore to avoid breaking other widgets
             
             DrawFPS(SCREEN_WIDTH - 100, 10);
 
             // Start the sliders below the text stats
-            Rectangle sliderBounds = { 500, 140, 300, 30 };
+            Rectangle sliderBounds = { 500, 50, 300, 30 };
             float sliderSpacing = 50;
 
             // Optional: Draw a heading in larger font
             DrawText("Boid Behaviour Weights", sliderBounds.x, sliderBounds.y - 40, 28, DARKGRAY);
 
-            // Make font size larger manually
-            //int labelFontSize = 22;
-            //int valueFontSize = 22;
 
             GuiSlider(sliderBounds,
                 TextFormat("Alignment (%.2f)", alignmentWeight),
